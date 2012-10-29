197b2da9d48cf0f4ad5372b14f498c91f7eec526
Clément <Clment@192.168.1.21>
2012-10-29 17:37:38 +0100

    Création de la méthode initialiser()

--- src/jeu/Partie.java
+++ src/jeu/Partie.java
@@ -1,12 +1,16 @@
 package jeu;
 
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 import carte.Famille;
 import carte.Quartier;
-import carte.personnage.Personnage;
+import carte.Merveille;
+import carte.personnage.*;
 
 /**
  * @author Bauchet Clément
@@ -110,7 +114,98 @@ public class Partie {
 	}
 	
 	private void initialiser() {
-/* ------------------------------------------------ A FAIRE ------------------------------- */
+		Famille religion = new Famille("Religion", Color.blue);
+		Famille noblesse = new Famille("Noblesse", Color.yellow);
+		Famille commerce = new Famille("Commerce", Color.green);
+		Famille militaire = new Famille("Militaire", Color.red);
+		Famille merveille = new Famille("Merveille", Color.magenta);
+		
+		pileQuartier.clear();
+		pileQuartier.add(new Quartier("Temple",religion,1));
+		pileQuartier.add(new Quartier("Temple",religion,1));
+		pileQuartier.add(new Quartier("Temple",religion,1));
+		pileQuartier.add(new Quartier("Eglise",religion,2));
+		pileQuartier.add(new Quartier("Eglise",religion,2));
+		pileQuartier.add(new Quartier("Eglise",religion,2));
+		pileQuartier.add(new Quartier("Eglise",religion,2));
+		pileQuartier.add(new Quartier("Monastère",religion,3));
+		pileQuartier.add(new Quartier("Monastère",religion,3));
+		pileQuartier.add(new Quartier("Monastère",religion,3));
+		pileQuartier.add(new Quartier("Cathédrale",religion,5));
+		pileQuartier.add(new Quartier("Cathédrale",religion,5));
+		
+		pileQuartier.add(new Quartier("Manoir",noblesse,3));
+		pileQuartier.add(new Quartier("Manoir",noblesse,3));
+		pileQuartier.add(new Quartier("Manoir",noblesse,3));
+		pileQuartier.add(new Quartier("Manoir",noblesse,3));
+		pileQuartier.add(new Quartier("Manoir",noblesse,3));
+		pileQuartier.add(new Quartier("Château",noblesse,4));
+		pileQuartier.add(new Quartier("Château",noblesse,4));
+		pileQuartier.add(new Quartier("Château",noblesse,4));
+		pileQuartier.add(new Quartier("Château",noblesse,4));
+		pileQuartier.add(new Quartier("Palais",noblesse,5));
+		pileQuartier.add(new Quartier("Palais",noblesse,5));
+		
+		pileQuartier.add(new Quartier("Taverne",commerce,1));
+		pileQuartier.add(new Quartier("Taverne",commerce,1));
+		pileQuartier.add(new Quartier("Taverne",commerce,1));
+		pileQuartier.add(new Quartier("Taverne",commerce,1));
+		pileQuartier.add(new Quartier("Taverne",commerce,1));
+		pileQuartier.add(new Quartier("Échoppe",commerce,2));
+		pileQuartier.add(new Quartier("Échoppe",commerce,2));
+		pileQuartier.add(new Quartier("Échoppe",commerce,2));
+		pileQuartier.add(new Quartier("Marché",commerce,2));
+		pileQuartier.add(new Quartier("Marché",commerce,2));
+		pileQuartier.add(new Quartier("Marché",commerce,2));
+		pileQuartier.add(new Quartier("Marché",commerce,2));
+		pileQuartier.add(new Quartier("Comptoir",commerce,3));
+		pileQuartier.add(new Quartier("Comptoir",commerce,3));
+		pileQuartier.add(new Quartier("Comptoir",commerce,3));
+		pileQuartier.add(new Quartier("Port",commerce,4));
+		pileQuartier.add(new Quartier("Port",commerce,4));
+		pileQuartier.add(new Quartier("Port",commerce,4));
+		pileQuartier.add(new Quartier("Hôtel de ville",commerce,5));
+		pileQuartier.add(new Quartier("Hôtel de ville",commerce,5));
+		
+		pileQuartier.add(new Quartier("Tour de guet",militaire,1));
+		pileQuartier.add(new Quartier("Tour de guet",militaire,1));
+		pileQuartier.add(new Quartier("Tour de guet",militaire,1));
+		pileQuartier.add(new Quartier("Prison",militaire,2));
+		pileQuartier.add(new Quartier("Prison",militaire,2));
+		pileQuartier.add(new Quartier("Prison",militaire,2));
+		pileQuartier.add(new Quartier("Caserne",militaire,3));
+		pileQuartier.add(new Quartier("Caserne",militaire,3));
+		pileQuartier.add(new Quartier("Caserne",militaire,3));
+		pileQuartier.add(new Quartier("Forteresse",militaire,5));
+		pileQuartier.add(new Quartier("Forteresse",militaire,5));
+		
+		pileQuartier.add(new Merveille("Cour des miracles",merveille,2,"Pour le décompte final des points, la cour des miracles est considérée comme un quartier de la couleur de votre choix. Vous ne pouvez pas utilisez cette capacité si vous avez construit la cour des miracles au dernier tour de jeu.",0));
+		pileQuartier.add(new Merveille("Donjon",merveille,3,"Le Donjon ne peut pas être détruit par le Condottière.",0));
+		pileQuartier.add(new Merveille("Laboratoire",merveille,5,"Une fois par tour, vous pouvez vous défausser d'une carte quartier de votre main et recevoir une pièce d'or en contrepartie",0));
+		pileQuartier.add(new Merveille("Manufacture",merveille,5,"Une fois par tour, vous pouvez payer trois pièces d'or pour piocher trois cartes.",0));
+		pileQuartier.add(new Merveille("Observatoire",merveille,3,"Si vous choisissez de piocher des cartes au début de votre tour, vous en piochez trois, en choisissez une et défaussez les deux autres.",0));
+		pileQuartier.add(new Merveille("Cimetière",merveille,5,"Lorsque le Condottière détruit un quartier, vous pouvez payer une pièce d'or pour le reprendre dans votre main. Vous ne pouvez pas faire cela si vous êtes vous-même Condottiere.",0));
+		pileQuartier.add(new Merveille("Bibliothèque",merveille,6,"Si vous choisissez de piocher des cartes au début de votre tour, vous en piochez deux et les conservez toutes les deux.",0));
+		pileQuartier.add(new Merveille("École de Magie",merveille,6,"Pour la perception des revenus, l'école de magie est considérée comme un quartier de la couleur de votre choix, elle vous rapporte donc si vous êtes, Roi, Evêque, Marchand ou Condottiere",0));
+		pileQuartier.add(new Merveille("Université",merveille,6,"Cette réalisation de prestige (nul n'a jamais compris à quoi pouvait bien servir une université) coûte six pièces d'or à bâtir mais vaut huit points dans le décompte de fin de partie.",2));
+		pileQuartier.add(new Merveille("Dracoport",merveille,6,"Cette réalisation de prestige (on n'a pas vu de dragon dans le Royaume depuis bientôt mille ans) coûte six pièces d'or à bâtir mais vaut huit points dans le décompte de fin de partie.",2));
+	
+		Collections.shuffle((List<Quartier>)(pileQuartier));
+		
+		pilePerso.clear();
+		pilePerso.add(new Assassin(null,this));
+		pilePerso.add(new Bailli(null,this));
+		pilePerso.add(new Magicien(null,this));
+		pilePerso.add(new Empereur(noblesse,this));
+		pilePerso.add(new Roi(noblesse,this));
+		pilePerso.add(new Abbe(religion,this));
+		pilePerso.add(new Marchand(commerce,this));
+		pilePerso.add(new Architecte(null,this));
+		pilePerso.add(new Condottiere(militaire,this));
+		pilePerso.add(new Reine(noblesse,this));
+		
+		Collections.shuffle((List<Personnage>)(pilePerso));
+		
 	}
 	
 	public void donnerOr(int nbPieces) {
